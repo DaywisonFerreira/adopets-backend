0c30dc565c6d813d0020c1958602a9263b701ffb
Daywison <daywison.leal@gmail.com>
2020-07-01 16:24:06 -0300

    add script to build

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const { Client } = require('pg');
 
 if(process.env.NODE_ENV === 'development') {
-  console.log('ambiente de desolvimento')
   module.exports = {
     dialect: 'postgres',
     host: process.env.DB_HOST,
@@ -16,7 +15,6 @@ if(process.env.NODE_ENV === 'development') {
     }
   }
 } else {
-  console.log('ambiente de producao')
   module.exports = {
     dialect: 'postgres',
     connectionString: process.env.DATABASE_URL,
