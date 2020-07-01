ab66531de71a3657c0284d80101020942efaa65f
Daywison <daywison.leal@gmail.com>
2020-07-01 18:32:46 -0300

    add script to build

--- src/config/database.js
+++ src/config/database.js
@@ -15,6 +15,7 @@ if(process.env.NODE_ENV === 'development') {
     }
   }
 } else {
+  console.log('env de producao' + process.env.DATABASE_URL)
   module.exports = {
     dialect: 'postgres',
     connectionString: process.env.DATABASE_URL
