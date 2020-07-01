d09c382d3d5c1aa15c4d8fd234ab5c8eb31b2d82
Daywison <daywison.leal@gmail.com>
2020-07-01 16:37:23 -0300

    add script to build

--- src/database/index.js
+++ src/database/index.js
@@ -6,6 +6,7 @@ import Product from '../app/models/Product';
 const env = process.env.NODE_ENV || 'development';
 const connection = require('../config/config.json')[env]
 
+console.log('A conexao é ' + connection)
 
 const models = [User, Product];
 
